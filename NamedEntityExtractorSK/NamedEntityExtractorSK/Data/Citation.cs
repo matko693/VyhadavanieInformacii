e47b1516b0c6734167a432c4bd317a3f00dc9975
Matej Chlebana <matej.chlebana@gmail.com>
2014-10-12 00:23:48 +0200

    Implement data class for Citations

---
+++ NamedEntityExtractorSK/NamedEntityExtractorSK/Data/Citation.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace NamedEntityExtractorSK.Data
+{
+	[DebuggerDisplay("Content = {Content}")]
+	public class Citation : KnowlegeData
+	{
+		#region Properties
+
+		public string[] Patterns = new string[] { "[Mm]eno[0-9]*", "[pP]riezvisko[0-9]*", "[vV]ydavate[lľ]", "[mM]iesto", "[mM]esto" };
+				
+		#endregion
+	}
+}
