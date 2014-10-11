25c6c14bbe324f3dde32348bc3b424b7645c20d4
Matej Chlebana <matej.chlebana@gmail.com>
2014-10-12 00:24:09 +0200

    Implement data class for Geoboxes

---
+++ NamedEntityExtractorSK/NamedEntityExtractorSK/Data/Geobox.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace NamedEntityExtractorSK.Data
+{
+	[DebuggerDisplay("Content = {Content}")]
+	public class Geobox : KnowlegeData
+	{
+		#region Properties
+
+		//locations
+		//mayor → person
+		public string[] Patterns = new string[] { "[Mm]eno[0-9]*", "[nN]ame", "[oO]ther_name", "[Cc]ountry", "[rR]egi[oó]n", "[mM]ayor"};
+
+		#endregion
+	}
+}
