f8613270df13f6c7d2f5e70097557a2a594e903d
Matej Chlebana <matej.chlebana@gmail.com>
2014-10-12 00:29:44 +0200

    Implement helper class WordUtils

---
+++ NamedEntityExtractorSK/NamedEntityExtractorSK/Utilities/WordUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NamedEntityExtractorSK.Data;
+
+namespace NamedEntityExtractorSK.Utilities
+{
+	public static class WordUtils
+	{
+		public static string[] Citacia = { "{{Citácia" };
+		public static string[] Infobox = { "{{Infobox" };
+		public static string[] Geobox = { "{{Geobox" };
+
+		public static string[] SearchBoxes = { "{{Citácia", "{{Infobox", "{{Geobox" };
+
+		private static char[] WhiteSpaces = new char[] { ' ', '\n', '\t' };
+
+		public static string TrimWhiteSpaces(this string text)
+		{
+			text = text.TrimStart(WhiteSpaces);
+			return text.TrimEnd(WhiteSpaces);
+		}
+
+		public static void TrimBoxes<T>(IEnumerable<string> splited, ref List<KnowlegeData> data)
+			where T : KnowlegeData
+		{
+			foreach (var item in splited)
+			{
+				var index = item.IndexOf("}}");
+
+				if (index != -1)
+				{
+					T instance = (T) Activator.CreateInstance(typeof(T));
+					instance.Content = item.Remove(index);
+					
+					data.Add(instance);
+				}
+			}
+		}
+	}
+}
